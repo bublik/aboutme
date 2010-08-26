3cbbffbf0a26c030803baeaa2e1092f4fb03f85e
Volochin Ruslan <rebisall@gmail.com>
2010-08-26 10:04:35 +0300

    updated style for pagination

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,5 +1,6 @@
 $(document).ready( function(){
-  $("button, input:submit, #header a").button();
+  $("button, input:submit, #header a, .pagination a, .pagination span").button();
+  $(".pagination em").button().addClass('ui-state-active');
   $("#header").buttonset().css('margin-top', '-4px');
 //  $(".alert, .error").button({
 //    icons: {
