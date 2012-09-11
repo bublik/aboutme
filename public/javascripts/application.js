c852be4330239cae40f83788489aa6e1d98a7aea
Ruslan Voloshin <rebisall@gmail.com>
2012-09-11 09:36:36 +0300

    update js

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -2,9 +2,12 @@ $(document).ready( function(){
   $("button, input:submit, #header a, .pagination a, .pagination span").button();
   $(".pagination em").button().addClass('ui-state-active');
   $("#header").buttonset().css('margin-top', '-4px');
-//  $(".tags a").buttonset({
-//    icons: {
-//      primary: 'ui-icon-tag'
-//   }
-//  });
+
+  $(document).ready(function(){
+    $('form[data-remote]').bind("ajax:before", function(){
+      for (instance in CKEDITOR.instances){
+        CKEDITOR.instances[instance].updateElement();
+      }
+    });
+  });
 });
