ff6957b27701c335cb47d8e25c577aa43b612673
Max Coder <mckenn@gmail.com>
2014-02-15 19:30:14 +0200

    remove categories helpers for navigation and move it to views

--- app/helpers/categories_helper.rb
+++ app/helpers/categories_helper.rb
@@ -1,23 +1,3 @@
 module CategoriesHelper
 
-  def navigation_categories
-    Category.all.collect do |category|
-      div_for(category) do
-        edit_links =  if user_signed_in? && current_user.admin?
-          link_to('', edit_category_path(category), class: 'glyphicon glyphicon-pencil text-success') +
-          link_to('', category, :confirm => 'Are you sure?', :method => :delete, class: 'glyphicon glyphicon-remove text-danger')
-        else
-          ''.html_safe 
-        end
-        link_to(h(category.title), category_messages_path(category), :title => category.title, class: "pull-left") + edit_links
-      end
-    end.join('').html_safe
-  end
-
-  def new_category_link
-    link_to(content_tag(:div, '',
-                        :class => 'ui-icon ui-icon-plus',
-                        :style => "float: left; margin-right: 0.3em; width: 15px"),
-            new_category_path)
-  end
 end
