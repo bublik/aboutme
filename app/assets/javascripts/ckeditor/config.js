93ecef1a3c528ae34fe08bc590be34987c1ee762
Max Coder <mckenn@gmail.com>
2014-02-17 12:28:42 +0200

    customize ckeditor

---
+++ app/assets/javascripts/ckeditor/config.js
@@ -0,0 +1,11 @@
+CKEDITOR.config.toolbar_mini =
+    [
+        ['Undo','Redo','-','Find','Replace','-','SelectAll','RemoveFormat'],
+        ['Bold','Italic','Underline','Strike','-','Subscript','Superscript'],
+        ['NumberedList','BulletedList','-','Outdent','Indent'],
+        ['JustifyLeft','JustifyCenter','JustifyRight','JustifyBlock'],
+        ['Link','Unlink'],
+        ['Image','Table','HorizontalRule'],
+        ['FontSize'],
+        ['TextColor','BGColor']
+    ];
